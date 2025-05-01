3ee3a42d27841c3b94239eab8d0d3537fba147b5
luis10barbo <luis10barbo2020@gmail.com>
2025-04-30 21:19:41 -0300

    feat: Adicionada licensa e loading em pagina de perfil

--- frontend/src/app/components/kyf-profile/kyf-profile.component.ts
+++ frontend/src/app/components/kyf-profile/kyf-profile.component.ts
@@ -10,9 +10,25 @@ import { KyfService } from '../../services/kyf/kyf.service';
   imports: [RouterModule]
 })
 export class KyfProfileComponent {
-  constructor(private kyfService: KyfService, private router: Router) {}
+  loading = true;
+  constructor(private kyfService: KyfService, private router: Router) {
+    kyfService.loadingSubject.subscribe((isLoading) => {
+      if (isLoading) {
+        return;
+      }  
+      if (kyfService.userSubject.getValue()) { 
+        return;
+      }
+      this.gotoMainPage();
+    })
+  }
+
+  gotoMainPage() {
+    this.router.navigate(["/kyf"]);
+  }
+
   doLogout() {
     this.kyfService.doLogout().subscribe();
-    this.router.navigate(["/kyf"]);
+    this.gotoMainPage();
   }
 }
