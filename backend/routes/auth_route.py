ea66da4d2202b0d5f2edae4f384903d7f4520519
luis10barbo <luis10barbo2020@gmail.com>
2025-05-04 18:22:13 -0300

    refactor: Removidas todas mencoes ao twitter

--- backend/routes/auth_route.py
+++ backend/routes/auth_route.py
@@ -11,25 +11,6 @@ from controller.auth import get_user
 
 
 auth_bp = Blueprint("auth", __name__)
-
-CLIENT_ID = 'Qm9TT2k0d2dDSy1GS0VpbjhpVUg6MTpjaQ'
-CLIENT_SECRET = 'fZe6PzvkALlnUo8k8QAJmLOKNukqDIVorj5m3e9GaxoDjrOklx'
-REDIRECT_URI = 'http://localhost:4200/auth/twitter'
-
-
-
-class OAuthResponse(TypedDict):
-    access_token: str
-    expires_in: int
-    refresh_token: str
-    scope: str
-    token_type: str
-
-
-class TwitterUserResponse(TypedDict):
-    id: str
-    username: str
-    url: str
     
 # TODO: PUT IN .ENV
 REDDIT_REDIRECT_URI = "http://localhost:4200/auth/reddit"
@@ -92,68 +73,3 @@ async def reddit_callback():
     # Run task to add furia related posts to account
     asyncio.create_task(add_reddit_furia_related_content())
     return ""
-
-# @auth_bp.route("/twitter", methods=["POST"])
-# async def twitter_callback():
-#     data = await request.get_json()
-#     code: str = data['code']
-#     code_verifier: str = data['code_verifier']
-
-#     # credentials = f"{CLIENT_ID}:{CLIENT_SECRET}"
-#     # b64_credentials = base64.b64encode(credentials.encode()).decode()
-
-#     token_url = 'https://api.twitter.com/2/oauth2/token'
-#     headers = {
-#         'Content-Type': 'application/x-www-form-urlencoded',
-#         }
-#     payload = {
-#         'grant_type': 'authorization_code',
-#         'code': code,
-#         'redirect_uri': REDIRECT_URI,
-#         'client_id': CLIENT_ID,
-#         'code_verifier': code_verifier
-#     }
-
-#     async with httpx.AsyncClient() as client:
-#         response = await client.post(token_url, data=payload, headers=headers)
-
-#     if response.status_code != 200:
-#         return jsonify({'error': response.text}), response.status_code
-
-#     response_json: OAuthResponse = response.json()
-
-#     headers = {
-#         'Authorization': f'Bearer {response_json["access_token"]}'
-#     }
-
-#     async with httpx.AsyncClient() as client:
-#         user_response = await client.get('https://api.twitter.com/2/users/me&user.fields=id,username,url', headers=headers)
-
-#     if user_response.status_code == 429:
-#         return "Conta twitter recebeu rate limit, utilize outras opções de login.", 429
-
-#     twitter_user: TwitterResponse[TwitterUserResponse] = user_response.json()
-
-    
-#     user = await get_user()
-#     if not user:
-#         return "Voce precisa estar autenticado para realizar essa acao", 401
-
-#     await db.socialmedialink.delete_many(where={'provider': {'equals': Providers.Twitter.value}, "userId": {'equals': user.id}})
-
-#     expiration_date = datetime.datetime.now() + datetime.timedelta(seconds=response_json['expires_in'])
-#     await db.socialmedialink.create({
-#             'accessToken': response_json['access_token'],
-#             'provider': Providers.Twitter.value,
-#             "providerUserId": twitter_user["data"]["id"],
-#             "providerUserNickname": twitter_user['data']['username'],
-#             'refreshToken': response_json["refresh_token"],
-#             'user': {'connect': {'id': user.id}},
-#             'expirationDate': expiration_date,
-#             'providerUserUrl': twitter_user['data']['url']
-#     })
-
-#     await twitter_update_user_furia_likes()
-
-#     return twitter_user
-#     # return jsonify(response.json())
